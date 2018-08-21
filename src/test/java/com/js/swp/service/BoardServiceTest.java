6e029fd67b79ff4fe995600e23ab6e6ea78b4361
jeongsso <rpsha33@naver.com>
2018-08-21 12:13:03 +0900

    BoardServiceTest modify

---
+++ src/test/java/com/js/swp/service/BoardServiceTest.java
@@ -0,0 +1,34 @@
+package com.js.swp.service;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import javax.inject.Inject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.js.swp.domain.Board;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/spring/**/root-context.xml"})
+
+public class BoardServiceTest extends BoardServiceImpl {
+
+	@Inject
+	private BoardService service;
+	
+	private static Logger logger = LoggerFactory.getLogger(BoardServiceTest.class);
+			
+	@Test
+	public void testRead() throws Exception {
+		fail("no implemented");
+		Board board = service.read(2);
+		logger.debug(board.toString());
+		assertEquals("제목", board.getTitle());
+	}
+}
